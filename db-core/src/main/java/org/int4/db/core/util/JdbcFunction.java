ef45a492a9962c7fedceb095aed355079bd60fb6
John Hendrikx <john.hendrikx@gmail.com>
2024-03-02 15:00:26 +0100

    Remove unnecessary override

--- db-core/src/main/java/org/int4/db/core/util/JdbcFunction.java
+++ db-core/src/main/java/org/int4/db/core/util/JdbcFunction.java
@@ -3,6 +3,4 @@ package org.int4.db.core.util;
 import java.sql.SQLException;
 
 public interface JdbcFunction<T, R> extends ThrowingFunction<T, R, SQLException> {
-  @Override
-  R apply(T t) throws SQLException;
 }
