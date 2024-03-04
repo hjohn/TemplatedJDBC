6d9a1957028407c9e77032a254a0cebd81cd7b14
John Hendrikx <john.hendrikx@gmail.com>
2024-03-04 06:35:30 +0100

    Fix javadoc

--- db-core/src/main/java/org/int4/db/core/util/ThrowingRunnable.java
+++ db-core/src/main/java/org/int4/db/core/util/ThrowingRunnable.java
@@ -2,12 +2,14 @@ package org.int4.db.core.util;
 
 /**
  * A runnable that can throw a checked exception.
+ *
+ * @param <X> the exception type
  */
 public interface ThrowingRunnable<X extends Throwable> {
-  
+
   /**
    * Runs an operation.
-   * 
+   *
    * @throws X when an error occurred
    */
   void run() throws X;
