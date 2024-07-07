0a94774439842aaedae960a2d943980c96f65825
John Hendrikx <john.hendrikx@gmail.com>
2024-07-07 17:15:14 +0200

    Remove junk that should not have been committed

--- db-core/src/main/java/org/int4/db/core/reflect/NameTranslator.java
+++ db-core/src/main/java/org/int4/db/core/reflect/NameTranslator.java
@@ -14,33 +14,4 @@ interface NameTranslator {
   };
 
   String toDatabaseName(String javaName);
-
-  public static void main(String[] args) {
-    String x = "thisIsATest: \"this_is_a_test\"\n"
-      + "EndWithNumber3: \"end_with_number_3\"\n"
-      + "3ThisStartWithNumber: \"3_this_start_with_number\"\n"
-      + "Number3InMiddle: \"number_3_in_middle\"\n"
-      + "Number3inMiddleAgain: \"number_3_in_middle_again\"\n"
-      + "MyUUIDNot: \"my_uuid_not\"\n"
-      + "HOLAMundo: \"hola_mundo\"\n"
-      + "holaMUNDO: \"hola_mundo\"\n"
-      + "with_underscore: \"with_underscore\"\n"
-      + "withAUniqueLetter: \"with_a_unique_letter\"\n"
-      + "with%SYMBOLAndNumber90: \"with_%_symbol_and_number_90\"\n"
-      + "http%: \"http_%\"\n"
-      + "123456789: \"123456789\"\n"
-      + "getUUIDAsString: \"123456789\"\n"
-//      + "     : \"     \"\n"
-      + "_: \"_\"\n"
-      + "__abc__: \"__abc__\"";
-
-    for(String line : x.lines().toList()) {
-      String[] parts = line.split(":");
-//      System.out.println(">>> " + parts[0] + " -> " + parts[1]);
-
-      if(!("\"" + UNDERSCORED.toDatabaseName(parts[0]) + "\"").equals(parts[1].trim())) {
-        System.out.println("Fails on: " + line + " with " + UNDERSCORED.toDatabaseName(parts[0]));
-      }
-    }
-  }
 }
